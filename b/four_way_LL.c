9e1e8cb9447ba30d66fa8c8500e88c5d62468881
james <jamesmedixon@gmail.com>
2020-12-31 13:13:49 +0000

    rotating rows

--- b/four_way_LL.c
+++ b/four_way_LL.c
@@ -19,11 +19,42 @@ struct board_structure{
 void add_to_board(struct board_structure *list, int x, int y, char data);
 void rotate_row(struct board_structure *list, int row_num);
 
-void free_board(struct board_structure *list);
+void free_board_content(struct board_structure *list);
 void base_board_setup(struct board_structure *list, int cols);
 struct node* query_board_structure(struct board_structure *list, int x, int y);
 struct board_structure* setup_board();
 
+void free_board_content(struct board_structure *list){
+    //for each base (starting at the final base)
+    //work way down the collumb, freeing each node along the way. then free base
+
+    
+    for(int i = 0; i <= list->cols; i++){
+        
+        struct node* current = list->head;
+        for(int ii = 0; ii < (list->cols - i); ii++){
+            current = current->right;
+        }
+        //current now at base of col
+
+        while (current->above != NULL)
+        {
+            current = current->above;
+        }
+        //current now at top of col
+
+        while (current->below != NULL)
+        {
+            current = current->below;
+            free(current->above);
+        }
+        //now freed the collumb
+        free(current);
+        //now freed the base of the collumb
+    }
+
+
+}
 
 struct board_structure* setup_board(){
     struct board_structure *mylist = malloc(sizeof(struct board_structure));
@@ -37,11 +68,11 @@ void base_board_setup(struct board_structure *list, int cols){
 
     char blank = '_';
 
-    for(int i = 0; i < cols; i++){
+    for(int i = 1; i < cols; i++){
         struct node* newnode = malloc(sizeof(struct node));
         newnode->data = blank;
         
-        if(previous == NULL){
+        if((previous == NULL)){
             list->head = newnode;
             previous = newnode;
             current = newnode->right;
@@ -69,9 +100,8 @@ void base_board_setup(struct board_structure *list, int cols){
         
     }
 
-
 struct node* query_board_structure(struct board_structure *list, int x, int y){
-
+    y++;
     //returnin list.head means the slot is on the borad but is free
     //returning list.head.next means that the x or y are out of bounds
     struct node* blank = list->head;
@@ -104,8 +134,6 @@ struct node* query_board_structure(struct board_structure *list, int x, int y){
     
     }
     
-    
-
 void add_to_board(struct board_structure *list, int x, int y, char data){
     if(query_board_structure(list, x,y) == list->head){
         //mean that the spot is free and is not off the board
@@ -133,10 +161,17 @@ void add_to_board(struct board_structure *list, int x, int y, char data){
         current->above = newnode;
         if(newnode->below->right != NULL){
             newnode->right = newnode->below->right->above;
+            if(newnode->below->right->above != NULL){
+                newnode->below->right->above->left = newnode;
+            }
+            
         }
 
         if(newnode->below->left != NULL){
-            newnode->right = newnode->below->right->above;
+            newnode->left = newnode->below->left->above;
+            if(newnode->below->left->above != NULL){
+                newnode->below->left->above->right = newnode;
+            }
         }
         
 
@@ -145,65 +180,84 @@ void add_to_board(struct board_structure *list, int x, int y, char data){
     }
 }
 
+void rotate_row(struct board_structure *list, int row_rotaion_num){
+    //!!!!!!!!!!!have to input rows as starting at 1 from bottom!!!!!!!! (beacause -0 == 0)
+    int direction = row_rotaion_num;
+    row_rotaion_num = abs(row_rotaion_num) -1;
+    //create the new board
+    struct board_structure *new_list = setup_board();
+    base_board_setup(new_list, list->cols+1);
+    new_list->rows = list->rows;
 
-void rotate_row(struct board_structure *list, int row_num){
-    for(int i= 0; i <= list->cols; i++){
-        struct node* current;
-        struct node* temp;
+    //go through the board row by row starting at the bottom
+    //add each to new board from left to right
+    //when get to the row that is shifting, place in new board either to the right or left by 2 (depending on sighn on row_num)
 
-        if(query_board_structure(list, i, row_num) != list->head){
-            current = query_board_structure(list, i, row_num);
-            while(current->right != NULL){
-                temp = current->right;
+    for(int row_num = 0; row_num <= list->rows; row_num++){
+        for(int col_num = 0; col_num <= list->cols ; col_num++){
+            int insert_x = col_num;
+            int insert_y = row_num;
 
-                current->above = temp->above;
-                current->below =temp->below;
-                current->right = temp->right;
-                current->left = temp->left;
-                temp = current;
-                i++;
+            if((row_num == row_rotaion_num && (abs(direction) == direction))){
+                insert_x ++;
+                if(insert_x > list->cols){
+                    insert_x = 0;
+                }
             }
-            
+            if((row_num == row_rotaion_num && (abs(direction) != direction))){
+                insert_x --;
+                if(insert_x < 0){
+                    insert_x = list->cols;
+                }
+            }
+
+            if(query_board_structure(list, col_num, row_num) != list->head){
+                add_to_board(new_list, insert_x, insert_y, query_board_structure(list, col_num, row_num)->data);
+            }
         }
-        //now have current being next to a gap to the right of it
+        
+    }
+    //free up everything from the old head
+    free_board_content(list);
+    
+    //now set the head of the hold list equal to the head of the new shifted list
+    list->head = new_list->head;
 
-        //need to find the top of the col to the right
+    //free the new_list structure as its content has been assighned to the old list
+    free(new_list);
 
-        //goes up y
-        struct node* r_col_interator = query_board_structure(list, i, 0)->right;
-        int top_r_col;
-        while (r_col_interator->above != NULL){
-            top_r_col++ ;
-            r_col_interator = r_col_interator->above;
-        }
-
-        current->below = r_col_interator;
-        current->right = r_col_interator->right->above;
-        current->left = r_col_interator->left->above;
-        current->above  = NULL;
-            
-        if(top_r_col + 1 == row_num){
-            i++;
-        }
-            
-        //if the top_r_col is one less than the row we are on, skip a col as we have just moved to the same row
-
-        struct node* top_of_right_col;
-
-        }
+    
+    
     }
 
 
 
-void main(){
+int main(){
     char p1 = "x";
   struct board_structure *mylist = setup_board();
   base_board_setup(mylist, 6);
 
   mylist->rows = 10;
   add_to_board(mylist, 1,1, p1);
-  add_to_board(mylist, 1,2, p1);
-  add_to_board(mylist, 2,3, p1);
+  add_to_board(mylist, 1,1, p1);
+
+  //add_to_board(mylist, 3,0, p1);
+  //add_to_board(mylist, 3,1, p1);
+
+
+  //add_to_board(mylist, 5,0, p1);
+  //add_to_board(mylist, 5,1, p1);
+  
+  
+
+  rotate_row(mylist, -1);
+
+  //to free everything:
+  //free_board_content(mylist);
+  //free(mylist);
+  
+
+  
 
   
   struct node* peice = query_board_structure(mylist, 1,1);
@@ -212,4 +266,5 @@ void main(){
   
 
   printf("done");
+  return 0;
 }
